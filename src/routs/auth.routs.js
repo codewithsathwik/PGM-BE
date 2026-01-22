52561f3f7e6693e7561baa32a68412f40261572f
codewithsathwik <sathvikms344@gmail.com>
2026-01-22 23:49:49 +0530

    added middlewares and validation to routs

--- src/routs/auth.routs.js
+++ src/routs/auth.routs.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import register from "../controllers/auth.controllers.js";
 
+import { userRegisterValidator } from "../validators/index.js";
+import { validate } from "../middlewares/validator.middleware.js";
+
 const router = Router();
 
-router.route("/register").post(register);
+router.route("/register").post(userRegisterValidator(),validate,register);
 
 export default router;
